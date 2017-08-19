819d0e4fb27348c50e290e07a42a54185554c145
nihanthkethireddy <nihanth.kethireddy@gmail.com>
2017-08-19 18:02:18 +0530

    Add files via upload

---
+++ ISAS/ISAS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAS
+{
+    interface A
+    {
+
+    }
+
+    interface B : A
+    {
+
+    }
+    class C : B
+    {
+
+    }
+    class E : C
+    {
+
+    }
+    class Program : E
+    {
+        static void Main(string[] args)
+        {
+            Program d = new Program();
+            Console.WriteLine(Class1.hai(d, typeof(A)));
+            Console.WriteLine(Class1.hai(d, typeof(A)) ? d : null);
+        }
+    }
+}
